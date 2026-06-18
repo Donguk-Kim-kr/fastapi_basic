cf705b7fcc26734cde371a044acfe47417cc74ec
loveyouduk <loveyouduk@naver.com>
2026-06-18 17:40:43 +0900

    2026-06-18. 29th FastAPI

--- database/db_connection.py
+++ database/db_connection.py
@@ -22,7 +22,7 @@ DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/tododb"
 engine = create_engine(DATABASE_URL, echo=True) # echo=True --> 실행되는 SQL을 터미널에 출력(디버깅용), 입문용이라 확인함, 실제로는 False
 
 # 세션 팩토리 생성
-SesstionFactory = sessionmaker(
+SessionFactory = sessionmaker(
     autocommit=False, # session.commit()를 직접 호출해야 DB에 반영
     autoflush=False, # flush : commit전에 SQL을 실행하는 중간 단계
     expire_on_commit=False, # commit 후에도 데이터가 메모리에 유지된다. (True라면 db 다시 조회)
